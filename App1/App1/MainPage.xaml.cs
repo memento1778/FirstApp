a54cc5a26ea0bf0553d0404a9378e52bdc229184
memento1778 <amleto1778@hotmail.com>
2016-08-20 19:52:22 +0200

    Ciao!

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using App1.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Il modello di elemento per la pagina base è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace App1
+{
+    /// <summary>
+    /// Pagina base che fornisce caratteristiche comuni alla maggior parte delle applicazioni.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// È possibile sostituirlo con un modello di visualizzazione fortemente tipizzato.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper viene utilizzato in oggi pagina per favorire la navigazione e 
+        /// la gestione del ciclo di vita dei processi
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Popola la pagina con il contenuto passato durante la navigazione. Vengono inoltre forniti eventuali stati
+        /// salvati durante la ricreazione di una pagina in una sessione precedente.
+        /// </summary>
+        /// <param name="sender">
+        /// Origine dell'evento. In genere <see cref="Common.NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Dati evento che forniscono il parametro di navigazione passato a
+        /// <see cref="Frame.Navigate(Type, Object)"/> quando la pagina è stata inizialmente richiesta e
+        /// un dizionario di stato mantenuto da questa pagina nel corso di una sessione
+        /// precedente. Lo stato è null la prima volta che viene visitata una pagina.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Mantiene lo stato associato a questa pagina in caso di sospensione dell'applicazione o se la
+        /// viene scartata dalla cache di navigazione.  I valori devono essere conformi ai requisiti di
+        /// serializzazione di <see cref="Common.SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">Origine dell'evento. In genere <see cref="Common.NavigationHelper"/></param>
+        /// <param name="e">Dati di evento che forniscono un dizionario vuoto da popolare con
+        /// uno stato serializzabile.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region Registrazione di NavigationHelper
+
+        /// I metodi forniti in questa sezione vengono utilizzati per consentire a
+        /// NavigationHelper di rispondere ai metodi di navigazione della pagina.
+        /// 
+        /// La logica specifica della pagina deve essere inserita nel gestore eventi per  
+        /// <see cref="Common.NavigationHelper.LoadState"/>
+        /// e <see cref="Common.NavigationHelper.SaveState"/>.
+        /// Il parametro di navigazione è disponibile nel metodo LoadState 
+        /// oltre allo stato della pagina conservato durante una sessione precedente.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
